c6c9418fafae39f74dcefc387328b60f77932c2f
= <=>
2023-08-15 09:15:19 -0300

    melhoria de codigo

--- src/jogoInterface/app.java
+++ src/jogoInterface/app.java
@@ -3,5 +3,16 @@ package jogoInterface;
 public class app {
     public static void main(String[] args) {
         
+        xadrez game = new xadrez();
+        game.jogar();
+
+        game.moverVert("Avançar");
+        game.moverVert("Avançar");
+        game.moverVert("Avançar");
+        game.moverVert("Avançar");
+        game.moverVert("Voltar");
+
+        game.Atacar("atacar Direita");
+
     }
 }
--- src/jogoInterface/xadrez.java
+++ src/jogoInterface/xadrez.java
@@ -2,6 +2,9 @@ package jogoInterface;
 
 public class xadrez implements jogo{
 
+    int x = 0;
+    int y = 0;
+
     @Override
     public void fechar() {
 
@@ -9,8 +12,48 @@ public class xadrez implements jogo{
 
     @Override
     public void jogar() {
-        System.out.println("Exibir menu");
-        System.out.println("Exibir video de jogadas de fundo");
+        System.out.println(x);
+        System.out.println(y);
     }
+
+    
+
+    public void moverVert(String direcao){
+        if(direcao == "Avançar"){
+            x++;
+            System.out.println("Avançando...");
+            System.out.println("posição de x é " + x);
+            System.out.println("posição de y é " + y);  
+        }else if(direcao == "Voltar"){
+            x--;
+            System.out.println("Avançando...");
+            System.out.println("posição de x é " + x);
+            System.out.println("posição de y é " + y); 
+        }else{
+            System.out.println("A posição de x");
+        }
+    }
+
+    //public void moverOrizont(String direcao2)
+
+    public void Atacar(String atacar){
+        if(atacar == "atacar Direita"){
+            x++;
+            y++;
+            System.out.println("atacando...");
+            System.out.println("posição de x é " + x);
+            System.out.println("posição de y é " + y); 
+        }else if (atacar == "atacar Esquerda"){
+            x++;
+            y--;
+
+            System.out.println("atacando...");
+            System.out.println("posição de x é " + x);
+            System.out.println("posição de y é " + y); 
+
+        }
+
+    }
+
     
 }
