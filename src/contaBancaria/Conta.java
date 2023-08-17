109bc66aa0a19d34dad6286ca2fff6df86cb9c6c
= <=>
2023-08-17 15:44:27 -0300

    new project

--- src/contaBancaria/Conta.java
+++ src/contaBancaria/Conta.java
@@ -3,6 +3,7 @@ package contaBancaria;
 public class Conta{
     
     private String nome;
+    private int SCore =9;
     protected double saldo;
     private int id;
 
@@ -34,7 +35,7 @@ public class Conta{
     }
 
     public void Saque(double saque){
-        if(saque > saldo){
+        if(saldo < saque){
             System.out.println("Saldo insuficiente!!");
         }else{
             saldo = saldo - saque;
@@ -43,6 +44,68 @@ public class Conta{
             System.out.println("Seu novo saldo é de R$" + saldo);
         }
     }
+    public void Tranferir(String nome, double valor){
+        if(saldo >= valor){
+            saldo = saldo - valor;
+            System.out.printf("Tranferencia para %s, no valor de %.2f \n", nome, valor);           
+        }else if(saldo < valor){
+            if(SCore >= 10){
+                //Vn =  Valor negativo
+                //Vj = valor com juros que tera que ser pago
+                double Vn = 0;
+                saldo = saldo - valor;
+                Vn = saldo;
+                //para cobrar juros encima do valor emprestado pelo banco
+                double Vj = Vn + (Vn * 8.5)/100;
+                System.out.println("Seu saldo foi insuficiente para fazer a tranferencia!!");
+                System.out.println("Avaliamos o seus Score, e percebemos que podemos financiar sua tranferencia!!");
+                System.out.printf("Vamos informar os procedimentos realizados: \n" + 
+                "Valor emprestado: %s \n" + 
+                "Valor a ser pago: %s \n",
+                Vn, Vj);
 
+            }else{
+                double Vf = valor - saldo;
+                System.out.println("Valor insufiente para a Transação...");
+                System.out.printf("FALTA R$%s PARA REALIZAR A ORDEM \n", Vf);
+            }
+        }
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSCore() {
+        return SCore;
+    }
+
+    public void setSCore(int sCore) {
+        SCore = sCore;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+    
     
 }
--- src/contaBancaria/app.java
+++ src/contaBancaria/app.java
@@ -4,12 +4,13 @@ public class app {
     public static void main(String[] args) {
         Conta c1 = new Conta("humberto", 1400, 74);
         c1.Saldo();
+        c1.Tranferir("humberto", 1500);
+        c1.Saldo();
 
-        Transferencias t1 = new Transferencias("humberto", 1400, 74);
-        t1.Tranferir("Beto", 700);
-        t1.Saldo();
-
+        /* 
+         //O valor do Investimento (salddo), corresponde ao valor que o usuario tem na corretora de investimento
         Investimento i1 = new Investimento("Humberto", 1400, 74);
-        i1.Investir("MXRF11", 338)  ;
+        i1.Investir("MXRF11", 338);
+        */
     }
 }
