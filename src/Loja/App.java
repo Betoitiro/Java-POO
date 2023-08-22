74314af711b2312f10c121d3be36d03779d377eb
= <=>
2023-08-21 23:07:39 -0300

    estoque

---
+++ src/Loja/App.java
@@ -0,0 +1,18 @@
+package Loja;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        Estoque e1 = new Estoque();
+
+        System.out.println("Quantos produtos deseja acrecentar: ");
+        int x = sc.nextInt();
+
+        
+
+    }
+}
---
+++ src/Loja/Estoque.java
@@ -0,0 +1,25 @@
+package Loja;
+
+public class Estoque {
+    private String Produto;
+    private double preco;
+    private double totalEstoque;
+
+    
+
+
+    public String getProduto() {
+        return Produto;
+    }
+    public void setProduto(String produto) {
+        Produto = produto;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    
+}
